ddb96637d74a1ed633f3edd5a9fec04ec46a177e
Aryan Sahu <aryansahu11233@gmail.com>
2026-08-17 21:40:27 +0530

    new

--- basic/HelloWorld.java
+++ basic/HelloWorld.java
@@ -1,5 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // integer
+        byte num = 5;
+        short n =1;
+        int i = 200;
+        long l = 2000;
+        System.out.println(num +"," +n +"," +i +"," +l);
+
+        // real 
+        float f =5.5f;
+        double d = 4.4;
+        System.out.println(f+"," +d);
     }
 }
