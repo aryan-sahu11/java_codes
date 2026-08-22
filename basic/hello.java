74e700c2489ec3ec467e54e0d77781d5d0660787
Aryan Sahu <aryansahu11233@gmail.com>
2026-08-22 18:15:14 +0530

    new

--- basic/hello.java
+++ basic/hello.java
@@ -1,5 +1,16 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        // int size = 27;
+        // String name = "Fido";
+        // hello myDog = new hello(name , size);
+        // x = size - 5;
+        // if (x<15) myDog.bark(8);
+
+        // while (x >3) {
+        //     myDog.play();
+        // }
     }
+
 }
+
